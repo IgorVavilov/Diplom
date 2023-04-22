1676264c601f89501b3834d454c8a286205a844b
sondarr <io.vavilov@gmail.com>
2023-04-22 23:04:14 +0700

    Регистрация категорий в админке, создание url.py и т.д.

--- securoko/shop/models.py
+++ securoko/shop/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Category(models.Model):
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, db_index=True, unique=True)
---
+++ securoko/shop/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.product_list, name='product_list'),
+    path('product_list/<str:category_slug>', views.product_list, name='product_list_by_category'),
+    path('product_list/<str:id>/<str:slug>', views.product_detail, name='product_detail'),
+]
