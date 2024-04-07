fe8413b2825e57f15104d4d795520f85976491a6
sondarr <io.vavilov@gmail.com>
2024-04-07 22:56:13 +0700

    Подготовка проекта в Деплой

--- securoko/securoko/settings.py
+++ securoko/securoko/settings.py
@@ -15,19 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-sxr4ne7o@a@!#dyra&$5b8j2f7n3c20$2cv9=cfzgr69jyj019"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -76,18 +63,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "securoko.wsgi.application"
 
-
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
@@ -117,7 +92,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR/"static"
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR/"media"
@@ -128,3 +102,8 @@ MEDIA_ROOT = BASE_DIR/"media"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CART_SESSION_ID = 'cart'
+
+try:
+    from .local_settings import *
+except ImportError:
+    from .prod_settings import *
